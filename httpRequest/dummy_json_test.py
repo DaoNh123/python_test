2ea8477dd943098fe5e9490b1c91a1e805076bdb
N.h.Dao <nhdao.it2.learn@gmail.com>
2025-07-17 09:24:27 +0700

    refresh dummy auth

--- httpRequest/dummy_json_test.py
+++ httpRequest/dummy_json_test.py
@@ -21,11 +21,24 @@ if __name__ == '__main__':
     # print(user)
     # print(user.accessToken)
 
-    # Get current User
-    current_user_endpoint = '/auth/me'
-    # wrong accessToken
-    get_wrong_user = get_current_user(root_url + current_user_endpoint, "123")
+    # # Get current User
+    # current_user_endpoint = '/auth/me'
+    # # wrong accessToken
+    # get_wrong_user = get_current_user(root_url + current_user_endpoint, "123")
+    # print('***get_wrong_user', get_wrong_user)
+    #
+    # # correct accessToken
+    # get_user = get_current_user(root_url + current_user_endpoint, user.accessToken)
+    # print('***get_user', get_user)
 
+    refresh_auth_endpoint = '/auth/refresh'
+    refresh_auth_response = get_refresh_auth_token(root_url + refresh_auth_endpoint, user.refreshToken, 30)
 
-    get_user = get_current_user(root_url + current_user_endpoint, user.accessToken)
-    print('***get_user', get_user)
+    print(refresh_auth_response)
+    newAccessToken, newRefreshToken = (
+        refresh_auth_response['accessToken'], refresh_auth_response['refreshToken']
+    )
+
+    print("  new auth token: ***  ")
+    print(newAccessToken, "\n", newRefreshToken)
+    print(newRefreshToken == user.refreshToken)
